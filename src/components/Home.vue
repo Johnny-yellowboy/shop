bbfe5248dc99a52bed357ad551015ffa7b6d654b
Johnny <Johnny-yellowboy.com>
2018-12-21 08:50:32 +0800

    登录

---
+++ src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <el-container class="home">
+    <el-header>
+      <div class="logo"></div>
+      <div class="title">
+        <h1>电商后台管理系统</h1>
+      </div>
+      <div class="logout">
+        <span>欢迎光临</span>
+        <a href="javascript:;">退出</a>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.home{
+  height: 100%;
+    .el-header{
+      background-color: #B3C0D1;
+      // color: #333;
+      text-align: center;
+      line-height: 60px;
+      display: flex;
+      .logo{
+        width: 180px;
+        background-image: url("../assets/logo.png");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: 180px;
+      }
+      .title{
+        flex: 1;
+        h1{
+          text-align: center;
+          line-height: 60px;
+          color: #fff;
+          font-size: 28px;
+        }
+      }
+      .logout{
+        width: 180px;
+        font-weight: 800;
+        line-height: 60px;
+        a{
+          color: rgb(2, 79, 202);
+        }
+      }
+  }
+
+  .el-aside {
+    background-color: rgb(103, 108, 115);
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+}
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,34 +1,81 @@
 <template>
   <div class="login">
-  <el-form ref="form" :model="form" label-width="80px" class="userform">
-    <img src="../assets/3812b31bb051f819e8e7a778d6b44aed2f73e7c6.jpg" alt="">
-    <el-form-item label="用户名">
+  <el-form ref="form" :model="form" :rules="rules" label-width="80px" class="userform" status-icon>
+    <img src="../assets/5a0b63366f390dd08ea65f2f16a06227.jpg" alt="">
+    <el-form-item label="用户名" prop="username">
       <el-input v-model="form.username"></el-input>
     </el-form-item>
-    <el-form-item label="密码">
-      <el-input v-model="form.password"></el-input>
+    <el-form-item label="密码"  prop="password">
+      <el-input v-model="form.password" type="password" ></el-input>
     </el-form-item>
   <el-form-item>
-    <el-button type="primary" @click="onSubmit">登录</el-button>
-    <el-button>取消</el-button>
+    <el-button type="primary" @click="login('form')">登录</el-button>
+    <el-button @click="resetForm('form')">重置</el-button>
   </el-form-item>
 </el-form>
   </div>
 </template>
 
 <script>
+// 导入axios
+import axios from 'axios'
 export default {
   data  () {
     return {
       form: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 9, message: '长度在 3 到 9 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
-    onSubmit  () {
-      console.log('submit!')
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
+    login (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          axios({
+
+            method: 'post',
+            url: 'http://localhost:8888/api/private/v1/login',
+            data: this.form
+
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              this.$message({
+                showClose: true,
+                message: '恭喜你，登录成功',
+                type: 'success'
+              })
+              // 储存token
+              localStorage.setItem('login', res.data.data.token)
+              // 跳转home首页
+              this.$router.push('/home')
+            } else {
+              this.$message({
+                showClose: true,
+                message: res.data.meta.msg,
+                type: 'error',
+                duration: 2000
+              })
+            }
+          })
+        } else {
+          return false
+        }
+      })
     }
   }
 }
@@ -53,10 +100,10 @@ export default {
         position: absolute;
         border-radius: 50%;
         left: 50%;
-        border-top: 10px solid rgb(238, 235, 235);
-        border-left: 2px solid #ccc;
-        border-right: 2px solid #ccc;
-        border-bottom: 6px solid rgb(238, 237, 237);
+        border-bottom: 10px solid rgb(199, 199, 199);
+        border-left: 8px solid rgb(235, 235, 235);
+        border-right: 8px solid rgb(235, 235, 235);
+        border-top: 10px solid rgb(199, 199, 199);
         top: -30%;
         transform: translateX(-50%);
       }
